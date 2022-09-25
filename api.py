933efaa5ebe1a558807afa0e64e017de818f88eb
eth59 <91006857+eth59@users.noreply.github.com>
2022-09-25 16:10:38 +0200

    Add files via upload

--- api.py
+++ api.py
@@ -1,22 +1,13 @@
 import requests
 import csv
+import sqlite3
 
 def generate_standings():
     payload={}
     headers = {
         'x-apisports-key': 'dc155322ba6c5a28850ada669a659f34'
-    }
-
-    url = "https://v3.football.api-sports.io/leagues"
-    response = requests.request("GET", url, headers=headers, data=payload)
-    data = response.json()
-    for league in data['response']:
-        if league['league']['name'] == 'Ligue 1' and league['country']['code'] == 'FR':
-            for season in league['seasons']:
-                if season['current']:
-                    id_ligue_1 = league['league']['id']
-                    
-    url = f"https://v3.football.api-sports.io/standings?league={id_ligue_1}&season=2022"
+    }            
+    url = f"https://v3.football.api-sports.io/standings?league=61&season=2022"
     response = requests.request("GET", url, headers=headers, data=payload)
     data = response.json()
     standings = data['response'][0]['league']['standings'][0]
@@ -37,3 +28,161 @@ def generate_standings():
             pts = line['points']
             last5 = line['form']
             writer.writerow([rank, club, mj, g, n, p, bp, bc, db, pts, last5])
+            
+
+def get_match(domicile, exterieur):
+    payload={}
+    headers = {
+        'x-apisports-key': 'dc155322ba6c5a28850ada669a659f34'
+    }
+    url = "https://v3.football.api-sports.io/fixtures?league=61&season=2022"
+    response = requests.request("GET", url, headers=headers, data=payload)
+    data = response.json()
+    for match in data['response']:
+        if (match['teams']['home']['name'] == domicile and
+            match['teams']['away']['name'] == exterieur):
+            return match
+        
+
+def get_team_list():
+    payload={}
+    headers = {
+        'x-apisports-key': 'dc155322ba6c5a28850ada669a659f34'
+    }
+    url = "https://v3.football.api-sports.io/teams?league=61&season=2022"
+    response = requests.request("GET", url, headers=headers, data=payload)
+    data = response.json()
+    team_list = []
+    for team in data['response']:
+        team_list.append(team['team']['name'])
+    return team_list
+
+
+def update_bdd():
+    try:
+        conn = sqlite3.connect('prono.db')
+        cursor = conn.cursor()
+        payload={}
+        headers = {
+            'x-apisports-key': 'dc155322ba6c5a28850ada669a659f34'
+        }
+        url = "https://v3.football.api-sports.io/fixtures?league=61&season=2022"
+        response = requests.request("GET", url, headers=headers, data=payload)
+        data = response.json()
+        for match in data['response']:
+            domicile = match['teams']['home']['name']
+            exterieur = match['teams']['away']['name']
+            cursor.execute("""
+            SELECT id_match, butDomicile, butExterieur, commence FROM matchs
+            WHERE domicile = ? AND exterieur = ?
+            """, (domicile, exterieur))
+            score = cursor.fetchone()
+            if score[3] == 0:
+                if match['fixture']['status']['short'] not in ['TBD', 'NS', 'PST']:                    
+                    cursor.execute("""
+                    UPDATE matchs
+                    SET commence = 1
+                    WHERE id_match = ?
+                    """, (score[0],))
+                    conn.commit()
+                
+            if (score[1] is None and score[2] is None
+                and match['goals']['home'] is not None 
+                and match['goals']['away'] is not None
+                and match['fixture']['status']['short'] == 'FT'):
+                
+                butD = match['goals']['home']
+                butE = match['goals']['away']
+                cursor.execute("""
+                UPDATE matchs
+                SET butDomicile = ?, butExterieur = ?
+                WHERE id_match = ?
+                """, (butD, butE, score[0]))
+                conn.commit()
+
+                cursor.execute("""
+                SELECT * FROM pronos WHERE id_match = ?
+                """, (score[0],))
+                pronos = cursor.fetchall()
+                for prono in pronos:
+                    if ((prono[2] == prono[3] and butD == butE) or
+                        (prono[2] < prono[3] and butD < butE) or
+                        (prono[2] > prono[3] and butD > butE)):
+                        points_win = 100
+                    else:
+                        points_win = 20
+                    if (abs(prono[2]-prono[3]) == abs(butD-butE)):
+                        points_win +=100
+                    if (prono[2]==butD and prono[3]==butE):
+                        points_win += 100
+                        
+                    cursor.execute("""
+                    SELECT points FROM users WHERE id_user = ?
+                    """, (prono[1],))
+                    points = cursor.fetchone()
+                    cursor.execute("""
+                    UPDATE users
+                    SET points = ?
+                    WHERE id_user = ?
+                    """, (points[0]+points_win, prono[1]))
+                    conn.commit()
+                    cursor.execute("""
+                    DELETE FROM pronos
+                    WHERE id_match = ? AND id_user = ?
+                    """, (prono[0], prono[1]))
+                    conn.commit()
+                        
+    except Exception as e:
+        raise e
+    finally:
+        conn.close()
+        
+
+def nickname(name):
+    if name.lower() == 'losc':
+        return 'Lille'
+    elif name.lower() == 'ol':
+        return 'Lyon'
+    elif name.lower() == 'om':
+        return 'Marseille'
+    elif name.lower() == 'psg':
+        return 'Paris Saint Germain'
+    elif name.lower() == 'tfc':
+        return 'Toulouse'
+    elif name.lower() == 'clermont':
+        return 'Clermont Foot'
+    elif name.lower() == 'brest' or name.lower() == 'stade brestois':
+        return 'Stade Brestois 29'
+    elif name.lower() == 'troyes':
+        return 'Estac Troyes'
+    elif name.lower() == 'rcl':
+        return 'Lens'
+    else:
+        return name.title()
+    
+    
+def get_remaining_requests():
+    payload={}
+    headers = {
+        'x-apisports-key': 'dc155322ba6c5a28850ada669a659f34'
+    }
+    url = "https://v3.football.api-sports.io/status"
+    response = requests.request("GET", url, headers=headers, data=payload)
+    data = response.json()
+    return data['response']['requests']['limit_day'] - data['response']['requests']['current']
+
+
+def get_points():
+    try:
+        conn = sqlite3.connect('prono.db')
+        cursor = conn.cursor()
+        
+        cursor.execute("""
+        SELECT pseudo, points FROM users
+        """)
+        res = cursor.fetchall()
+    except Exception as e:
+        raise e
+    finally:
+        conn.close()
+    return res
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from doctest import master
+import sqlite3
 import discord
 import interactions
 import csv
-from api import generate_standings
+from api import *
 
 TOKEN = "MTAyMzE4MjY3ODY4Nzc1NjMwOA.GBrX3H.Z5kHoLFq3oBxu4xdTjmypRtiSifMXges3EolLk"
 MY_GUILD = discord.Object(id=1010577304134623273)
@@ -48,7 +48,164 @@ async def on_ready():
 )
 async def prono(ctx: interactions.CommandContext, domicile: str,
                 exterieur: str, butdomicile: int, butexterieur: int):
-    await ctx.send(f"{domicile} {butdomicile}-{butexterieur} {exterieur}")
+    dom = nickname(domicile)
+    ext = nickname(exterieur)
+    # try:
+    #     update_bdd()
+    # except Exception as e:
+    #     print(f"\n\n\n")
+    #     print(type(e))
+    #     print(f"Erreur lors d'une tentative d'ajout d'un pronostic (maj BDD) :\n {e}\n\n\n")
+    #     await ctx.send("Une erreur inconnue s'est produite lors de la tentative d'actulasation des matchs.")
+    # else:
+    try:
+        conn = sqlite3.connect('prono.db')
+        cursor = conn.cursor()
+        
+        cursor.execute("""
+        SELECT id_match, domicile, exterieur, commence FROM matchs
+        """)
+        match_list = cursor.fetchall()
+        for match in match_list:
+            if match[1] == dom and match[2] == ext:
+                id_match = match[0]
+                if match[3] == 1:
+                    raise ValueError
+                break
+            
+        id_user =int(str(ctx.user.id))
+        cursor.execute("""
+        SELECT id_user FROM users WHERE id_user = ?
+        """,[id_user])
+        users_list = cursor.fetchall()
+        if users_list == []:
+            user_values = (
+                id_user,
+                ctx.user.username,
+                ctx.user.discriminator
+            )
+            cursor.execute("""
+            INSERT INTO users (id_user, pseudo, discriminator) VALUES (?, ?, ?)            
+            """, user_values)
+            conn.commit()
+        
+        values = (id_match, id_user, butdomicile, butexterieur)
+        cursor.execute("""
+        INSERT INTO pronos (id_match, id_user, butDomicile, butExterieur)
+        VALUES (?, ?, ?, ?)
+        """, values)
+        conn.commit()
+        await ctx.send("Votre pronostic a été pris en compte.")
+    except sqlite3.IntegrityError:
+        await ctx.send("Vous avez déjà pronostiqué ce match.")
+    except UnboundLocalError:
+        await ctx.send("Ce match n'existe pas.")
+    except ValueError:
+        await ctx.send("Espèce de tricheur ! On ne parie pas sur un match qui est fini !")
+    except Exception as e:
+        print(f"\n\n\n")
+        print(type(e))
+        print(f"Erreur lors d'une tentative d'ajout d'un pronostic :\n {e}\n\n\n")
+        conn.rollback()
+        await ctx.send("Une erreur inconnue s'est produite.")
+    finally:
+        conn.close()
+    
+    
+@bot.command(
+    name="liste_equipe",
+    description="Donne la liste de toutes les équipes",
+    scope=MY_GUILD.id
+)
+async def liste_equipe(ctx: interactions.CommandContext):
+    team_list = get_team_list()
+    message = "```\n"
+    for team in team_list:
+        if team == 'Lille':
+            message += f"Lille (ou LOSC)\n"
+        elif team == 'Lyon':
+            message += f"Lyon (ou OL)\n"
+        elif team == 'Marseille':
+            message += f"Marseille (ou OM)\n"
+        elif team == 'Paris Saint Germain':
+            message += f"Paris Saint Germain (ou PSG)\n"
+        elif team == 'Toulouse':
+            message += f"Toulouse (ou TFC)\n"
+        elif team == 'Clermont Foot':
+            message += f"Clermont Foot (ou Clermont)\n"
+        elif team == 'Stade Brestois 29':
+            message += f"Stade Brestois 29 (ou Brest)\n"
+        elif team == 'Estac Troyes':
+            message += f"Estac Troyes (ou Troyes)\n"
+        elif team == 'Lens':
+            message += f"Lens (ou RCL)\n"
+        else:
+            message += f"{team}\n"
+    message += "```"
+    await ctx.send(message)
+    
+@bot.command(
+    name="info_match",
+    description="Donne quelques infos sur un match",
+    scope=MY_GUILD.id,
+    options=[
+        interactions.Option(
+            name="domicile",
+            description="Nom de l'équipe domicile",
+            type=interactions.OptionType.STRING,
+            required=True
+        ),
+        interactions.Option(
+            name="exterieur",
+            description="Nom de l'équipe extérieure",
+            type=interactions.OptionType.STRING,
+            required=True
+        )
+    ]
+)
+async def info_match(ctx: interactions.CommandContext, domicile: str, exterieur: str):
+    dom = nickname(domicile)
+    ext = nickname(exterieur)
+    match = get_match(dom, ext)
+    if match == None:
+        await ctx.send("Ce match n'existe pas.")
+    else:
+        message = '```\n'
+        if match['fixture']['status']['short'] in ['P', 'PEN']:
+            message += f"{dom} {match['goals']['home']} ({match['score']['penalty']['home']})-"
+            message += f"({match['score']['penalty']['away']}) {match['goals']['away']} {ext}\n"
+        else:
+            message += f"{dom} {match['goals']['home']}-{match['goals']['away']} {ext}\n"
+        message += f"Statut : {match['fixture']['status']['long']} ({match['fixture']['status']['short']})\n"
+        message += f"Temps écoulé : {match['fixture']['status']['elapsed']}\n"
+        date = match['fixture']['date']
+        message += f"Coup d'envoi : le {date[8:10]}-{date[5:7]}-{date[:4]} à {date[11:16]} {match['fixture']['timezone']}\n"
+        message += f"Lieu du match : {match['fixture']['venue']['name']} ({match['fixture']['venue']['city']})\n"
+        message += f"Arbitré par : {match['fixture']['referee']}\n"
+        message += "```"
+        await ctx.send(message)
+        
+@bot.command(
+    name="points",
+    description="Affiche les points des utilisateurs.",
+    scope=MY_GUILD.id
+)
+async def points(ctx: interactions.CommandContext):
+    try:
+        res = get_points()
+    except Exception as e:
+        print(f"\n\n\n")
+        print(type(e))
+        print(f"Erreur lors de l'obtention des points :\n {e}\n\n\n")
+        
+    message = "```\n+-----------------+------- +\n"
+    message += "| Pseudo          | Points |\n"
+    message += "+-----------------+------- +\n"
+    for row in res:
+        message += f"| {row[0]:<15} | {row[1]:<6} |\n"
+    message += "+-----------------+--------+\n```"
+    await ctx.send(message)
+    
 
 @bot.command(
     name="classement",
@@ -98,6 +255,60 @@ async def classement(ctx: interactions.CommandContext):
         if i==0:
             message += "+----+-----------------+----+----+----+----+----+----+-----+-----+------------+\n"
     message += "\u001b[0m+----+-----------------+----+----+----+----+----+----+-----+-----+------------+\n```"
+    await ctx.send(message)
+    
+@bot.command(
+    name="actualise",
+    description="Met à jour la base de données des matchs.",
+    scope=MY_GUILD.id
+)
+async def actualise(ctx: interactions.CommandContext):
+    await ctx.defer()
+    try:
+        update_bdd()
+    except Exception as e:
+        print(f"\n\n\n")
+        print(type(e))
+        print(f"Erreur lors de la mise à jour de la BDD :\n {e}\n\n\n")
+        await ctx.send("Une erreur inconnue s'est produite.")
+    else:
+        await ctx.send("Les matchs ont bien été actualisés.")
+
+@bot.command(
+    name='requetes_restantes',
+    description="Affiche le nombre de requêtes restantes pour la journée",
+    scope=MY_GUILD.id
+)
+async def requetes_restantes(ctx: interactions.CommandContext):
+    answer = get_remaining_requests()
+    await ctx.send(f"Il vous reste {answer} requêtes pour la journée.")
+
+@bot.command(
+    name='help',
+    description='Aide les noobs.',
+    scope=MY_GUILD.id
+)
+async def help(ctx: interactions.CommandContext):
+    message = "```\n"
+    message += "Salut moi c'est Footbot, le meilleur bot de pronostic footballistique (ou plutôt de Ligue 1 car je ne fais pas plus).\n"
+    message += "La liste des différentes choses que je maîtrise :\n"
+    message += "- classement : Affiche le classement actuel de la ligue 1\n"
+    message += "- liste_equipe : Affiche la liste des équipes (dans le format pris en charge par les commandes)\n"
+    message += "- info_match : Affiche quelques infos plus ou moins utiles sur un match\n"
+    message += "- prono (et pas porno) : Le plus important ! Permez de pronostiquer un match\n"
+    message += "- actualise : Permet d'actualiser la base de données et donc de vous donner des points (si vous avez pronostiqué bien sûr)\n"
+    message += "- points : Affiche les points de chaque utilisateur\n"
+    message += "- requetes_restantes : Affiche le nombre de requêtes restantes (cf plus bas)\n"
+    message += "\nLes points sont attribués de cette manière :\n"
+    message += "- 20 points si vous avez pronostiqué avec le cul\n"
+    message += "- 100 points si vous avez pronostiqué le bon vainqueur (ou le nul)\n"
+    message += "- 100 points bonus si vous avez la bonne différence de buts\n"
+    message += "- 100 points bonus si vous avez le score exact (cumulable avec la différence de buts)\n"
+    message += "\nQu'est-ce que les requêtes ?\n"
+    message += "L'API que j'utilise est limitée à 100 requêtes par jour. "
+    message += "Il faut donc retenir que chaque commande correspond à 1 requête, "
+    message += "à l'exception de requetes_restantes qui est gratuite. N'abuse pas de moi jeune padawan !\n"
+    message += "\nJe te souhaite de m'utiliser comme il se doit ! ;)\n```"
     await ctx.send(message)
     
 bot.start()
